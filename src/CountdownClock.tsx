1f1fb75f90c065b714a2c31668722766ec69ea12
Michael Siebert <michael.siebert@samedi.de>
2019-05-09 19:45:32 +0200

    add blinking on <60s

--- src/CountdownClock.tsx
+++ src/CountdownClock.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import moment, { Moment } from 'moment'
+import classnames from 'classnames'
 
 interface CountdownClockProps {
   targetTime: Moment
@@ -7,14 +8,18 @@ interface CountdownClockProps {
 }
 
 const CountdownClock: React.FC<CountdownClockProps> = ({ targetTime, now }) => {
-  const remainingTime = moment.duration(targetTime.diff(now))
+  let remainingTime = moment.duration(targetTime.diff(now))
+  if (remainingTime.asSeconds() < 0) {
+    remainingTime = moment.duration(0)
+  }
+  const shouldBlink = remainingTime.asSeconds() < 60
 
   const remainingParts = [remainingTime.hours(), remainingTime.minutes(), remainingTime.seconds()]
 
   const sep = ':'
 
   return (
-    <div className="CountdownClock">
+    <div className={classnames('CountdownClock', shouldBlink ? 'blinking' : false)}>
       88:88:88
       <div className="front">{remainingParts.map(n => `${n}`.padStart(2, '0')).join(sep)}</div>
     </div>
